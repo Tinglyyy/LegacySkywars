8789e5e78fa98a63de93580020a45f3a8d79ff7e
Tinglyyy <Unverwunderbar@proton.me>
2025-05-19 15:37:26 +0200

    Fist Commit

---
+++ src/main/java/de/unverwunderbar/legacy/legacyskywars/LegacySkywars.java
@@ -0,0 +1,17 @@
+package de.unverwunderbar.legacy.legacyskywars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LegacySkywars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
